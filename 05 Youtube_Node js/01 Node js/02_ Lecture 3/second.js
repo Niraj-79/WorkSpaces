d2c34014837935c4374d2f05663c94d13584588f
Niraj79 <nirajkumar50078@gmail.com>
2024-09-23 23:19:44 +0530

    Niraj

--- 05 Youtube_Node js/01 Node js/02_ Lecture 3/second.js
+++
@@ -1,7 +0,0 @@
-console.log("KG Codiing is the best");
-
-const fs = require("fs");
-fs.writeFile("second.txt", "Writing file", (err) => {
-  if (err) console.log("Error occurred");
-  else console.log("File Written Successfully");
-});
