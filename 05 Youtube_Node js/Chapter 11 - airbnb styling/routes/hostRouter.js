168f509632017f041bf7b591a43f0895f817b9bc
Niraj79 <nirajkumar50078@gmail.com>
2024-10-27 07:51:41 +0530

    Chapter 11

---
+++ 05 Youtube_Node js/Chapter 11 - airbnb styling/routes/hostRouter.js
@@ -0,0 +1,20 @@
+// Core Modules
+const path = require("path");
+
+// External Module
+const express = require("express");
+const hostRouter = express.Router();
+
+// Local Module
+const rootDir = require("../utils/pathutil");
+
+
+hostRouter.get("/add-home", (req, res, next) => {
+  res.sendFile(path.join(rootDir, "views", "addHome.html"));
+});
+
+hostRouter.post("/add-home", (req, res, next) => {
+  res.sendFile(path.join(rootDir, "views", "homeAdded.html"));
+});
+
+module.exports = hostRouter;
